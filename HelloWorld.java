fb88e8e07ce65af4aca3590688e393efc12af9cb
sahilvadia <sahilvadia7776@gmail.com>
2024-02-01 22:51:42 +0530

    first commit

---
+++ HelloWorld.java
@@ -0,0 +1,35 @@
+public class HelloWorld {
+ 
+     public static void main(String[] args) {
+        System.out.println("Hello");
+
+      //primitive data types
+
+      //integer
+        int i = 9;
+        byte by =127;
+        short s = 558;
+        long l = 5853l;
+
+      //float
+        float f = 5.7f;
+        double d = 5.6;
+
+      //char
+        char c= 's';
+
+      //boolean
+        boolean b =true;
+ 
+        System.out.println(i);
+        System.out.println(by);
+        System.out.println(s);
+        System.out.println(l);
+        System.out.println(f);
+        System.out.println(d);
+        System.out.println(c);
+        System.out.println(b);
+
+
+     }
+}
